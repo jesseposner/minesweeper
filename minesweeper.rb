9574c66558cad5f064f7f6625c2eb80d7519eda6
App Academy Student <student@appacademy.io>
2016-03-10 17:43:19 -0800

    adds Game class

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,7 +1,33 @@
 require 'byebug'
 
-class Board
+class Game
   def initialize(size: 9, bombs: 10)
+    @board = Board.new(size, bombs)
+  end
+
+  def get_guess
+    puts "What's your guess?"
+    guess = parse_guess(gets.chomp)
+    until valid_guess?(guess)
+      puts "What's your guess?"
+      guess = parse_guess(gets.chomp)
+    end
+  end
+
+  def parse_guess(guess)
+    guess.split(",").map!(&:to_i)
+  end
+
+  def valid_guess?(guess)
+    guess.length == 2 &&
+      guess.all? { |el| (0...@board.size).include?(el) }
+  end
+
+end
+
+
+class Board
+  def initialize(size, bombs)
     @grid = Array.new(size) { Array.new(size) { Tile.new } }
     assign_bombs(bombs)
   end
@@ -11,8 +37,7 @@ class Board
     until bomb_positions.length == bombs
       x = rand(@grid.length)
       y = rand(@grid.length)
-      bomb_positions << [x,y]
-      bomb_positions.uniq!
+      bomb_positions << [x,y] unless bomb_positions.include?([x,y])
     end
     bomb_positions.each { |pos| self[pos].set_bomb }
   end
@@ -20,6 +45,18 @@ class Board
   def [](pos)
     x, y = pos
     @grid[x][y]
+  end
+
+  def reveal(pos)
+    game_over if self[pos].bomb
+  end
+
+  def game_over
+
+  end
+
+  def size
+    @grid.length
   end
 end
 
